c3f61c84eb00ad5dfa77ebcb0622ffc70621a829
John S. Anvik <j.anvik@uleth.ca>
2026-02-04 10:19:04 -0700

    - Finished grid setup

--- Assignment 1/grid.cpp
+++ Assignment 1/grid.cpp
@@ -1,10 +1,10 @@
 /**
 * @author John S. Anvik (j.anvik@uleth.ca)
-* @date 2026-01-30
+* @date 2026-02-04
 */
 /* Given a grid similar to the one in Figure 1. You may prompt the user to: enter: 
 - the width and the height of the grid, 
-- the start and the goal states, and 
+- the start and the goal states, and  
 - the forbidden squares.  
 Implement each of the following searches and show the solutions:  
 (a) A depth-first search from S to G, given that the order of the operators you will test
@@ -26,22 +26,9 @@ const std::string STARTING_SQUARE = "S";
 const std::string GOAL_SQUARE = "G";
 const std::string FORBIDDEN_SQURE = "X";
 
-void print_grid(std::vector<std::string> g) {
-    std::cout << std::endl;
-    for (std::string s : g) {std::cout << s << std::endl;}
-}
 
-/**
- * @function 
- * @param x x coordinate
- * @param y y coordinate
- * @param g grid (vertical vector of horizontal strings)
-*/
-bool valid_square(int x, int y, std::vector<std::string> g) {
-    // return (x > 0 && y > 0 && x < g.size() && y < g.at(0).length());
-    std::cout << std::to_string(g[x].at(y)) << std::endl;
-    return (x > 0 && y > 0 && x < g.size() && y < g.at(0).length() && std::to_string(g[x].at(y)) == EMPTY_SQUARE);
-}
+void print_grid(std::vector<std::string> g);
+bool valid_square(int x, int y, std::vector<std::string> g);
 
 int main() {
     int x, y;
@@ -55,9 +42,9 @@ int main() {
     // Grid spaces are 1-indexed (vector row 0 and string index 0 print grid index numbers)
     std::cout << "\n Enter grid width and height in the following form: x y ";
     std::cin >> x >> y;
-    for (int i = 0; i < x+1; ++i) {
+    for (int i = 0; i <= y; ++i) {
         std::string v = std::to_string(i);
-        for (int j = 0; j < y; ++j) {
+        for (int j = 0; j < x; ++j) {
             if (i == 0) {v.append(std::to_string(j+1));}
             else {v.append(EMPTY_SQUARE);}
         }
@@ -66,28 +53,13 @@ int main() {
     print_grid(grid);
     x = 0; y = 0; // Reset x and y to invalid coordinates
     
-    while (entering_forbidden_squares) {
-        std::cout << "\n To place a forbidden square, enter coordinates in the following form: x y \n or enter -1 to place no more forbidden squares \n";
-        std::cin >> x >> y;
-        if (x !=-1 && y!=-1) {
-            if (valid_square(x, y, grid)) {
-                grid[x].replace(y, 1, "X");
-            } else {
-                std::cout << "INVALID SQUARE" << std::endl;
-            }
-        } else {entering_forbidden_squares = false;}
-    }
-
-    print_grid(grid);
-    x = 0; y = 0; // Reset x and y to invalid coordinates
-    
-        // Add starting square in grid
+    // Add starting square in grid
     while (!valid_square(x, y, grid)) {
         std::cout << "\n Enter starting coordinates in the following form: x y ";
         std::cin >> x >> y; 
         // Check for valid starting square coordinates, then place starting square
         if (valid_square(x, y, grid)) {
-            grid[x].replace(y, 1, STARTING_SQUARE);
+            grid[y].replace(x, 1, STARTING_SQUARE);
         } else {std::cout << "INVALID STARTING SQUARE\n";}
     }
     print_grid(grid);
@@ -98,15 +70,50 @@ int main() {
         std::cin >> x >> y; 
         // Check for valid goal coordinates, then place goal square
         if (valid_square(x, y, grid)) {
-            grid[x].replace(y, 1, GOAL_SQUARE);
+            grid[y].replace(x, 1, GOAL_SQUARE);
         } else {std::cout << "INVALID GOAL SQUARE\n";}
     }
     print_grid(grid);
     x = 0; y = 0; // Reset x and y to invalid coordinates
     
+    // Enter forbidden squares
+    while (entering_forbidden_squares) {
+        std::cout << "\n To place a forbidden square, enter coordinates in the following form: x y \n or enter -1 -1 to place no more forbidden squares \n";
+        std::cin >> x >> y;
+        if (x !=-1 && y!=-1) {
+            if (valid_square(x, y, grid)) {
+                grid[y].replace(x, 1, "X");
+                print_grid(grid);
+            } else {
+                std::cout << "INVALID SQUARE" << std::endl;
+            }
+        } else {entering_forbidden_squares = false;}
+    }
+
+    x = 0; y = 0; // Reset x and y to invalid coordinates
+
     while(!goal) {
         
     }
 
     return 0;
+}
+
+/**
+ * @return true if square is valid to move to, otherwise false
+ * @param g grid (vertical vector of horizontal strings)
+ */
+bool valid_square(int x, int y, std::vector<std::string> g) {
+    return (x > 0 && y > 0 && y < g.size() && x < g.at(0).length() && g[y].substr(x, 1) != "X");
+}
+
+/**
+ * @function 
+ * @param x x coordinate
+ * @param y y coordinate
+ * @param g grid (vertical vector of horizontal strings)
+*/
+void print_grid(std::vector<std::string> g) {
+    std::cout << std::endl;
+    for (std::string s : g) {std::cout << s << std::endl;}
 }
