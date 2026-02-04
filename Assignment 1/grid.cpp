c5de0914acbf8b756ff1fc6563c1b88adfd45578
John S. Anvik <j.anvik@uleth.ca>
2026-02-03 19:38:00 -0700

    - Continued implementing board setup - Finished DFS tree for question 3b

--- Assignment 1/grid.cpp
+++ Assignment 1/grid.cpp
@@ -19,43 +19,93 @@ reached?  */
 #include <vector>
 #include <string>
 
+// CHaracters to represent grid squares
+bool entering_forbidden_squares = true;
+const std::string EMPTY_SQUARE = "-";
+const std::string STARTING_SQUARE = "S";
+const std::string GOAL_SQUARE = "G";
+const std::string FORBIDDEN_SQURE = "X";
+
+void print_grid(std::vector<std::string> g) {
+    std::cout << std::endl;
+    for (std::string s : g) {std::cout << s << std::endl;}
+}
+
+/**
+ * @function 
+ * @param x x coordinate
+ * @param y y coordinate
+ * @param g grid (vertical vector of horizontal strings)
+*/
+bool valid_square(int x, int y, std::vector<std::string> g) {
+    // return (x > 0 && y > 0 && x < g.size() && y < g.at(0).length());
+    std::cout << std::to_string(g[x].at(y)) << std::endl;
+    return (x > 0 && y > 0 && x < g.size() && y < g.at(0).length() && std::to_string(g[x].at(y)) == EMPTY_SQUARE);
+}
+
 int main() {
     int x, y;
-    std::vector<std::vector<char>> grid;
+    std::vector<std::string> grid = {};
     bool goal = false;
-    bool entering_forbidden_squares = true;
     
     // prompt the user to enter the width and the height of the grid, the start and the goal states, and the forbidden squares.
+    
+    // Initialize Grid
+    // A vertical vector of horizontal strings
+    // Grid spaces are 1-indexed (vector row 0 and string index 0 print grid index numbers)
     std::cout << "\n Enter grid width and height in the following form: x y ";
     std::cin >> x >> y;
-    for (int i = x; i < x; ++i) {
-        std::vector<char> v;
-        for (int j = y; j < y; ++j) {
-            v.push_back('O');
+    for (int i = 0; i < x+1; ++i) {
+        std::string v = std::to_string(i);
+        for (int j = 0; j < y; ++j) {
+            if (i == 0) {v.append(std::to_string(j+1));}
+            else {v.append(EMPTY_SQUARE);}
         }
         grid.push_back(v);
     }
-    std::cout << "\n Enter starting coordinates in the following form: x y ";
-    std::cin >> x >> y; 
-    grid[x-1][y-1] = 'S';
-    std::cout << "\n Enter goal coordinates in the following form: x y ";
-    std::cin >> x >> y; 
-    grid[x-1][y-1] = 'G';
+    print_grid(grid);
+    x = 0; y = 0; // Reset x and y to invalid coordinates
+    
     while (entering_forbidden_squares) {
         std::cout << "\n To place a forbidden square, enter coordinates in the following form: x y \n or enter -1 to place no more forbidden squares \n";
         std::cin >> x >> y;
         if (x !=-1 && y!=-1) {
-            if (grid[x-1][y-1] != 'S' && grid[x-1][y-1] != 'G') {
-                grid[x-1][y-1] = 'X';
+            if (valid_square(x, y, grid)) {
+                grid[x].replace(y, 1, "X");
             } else {
-                std::cout << "INVALID SQUARE: " << grid[x-1][y-1] << std::endl;
+                std::cout << "INVALID SQUARE" << std::endl;
             }
         } else {entering_forbidden_squares = false;}
     }
 
+    print_grid(grid);
+    x = 0; y = 0; // Reset x and y to invalid coordinates
+    
+        // Add starting square in grid
+    while (!valid_square(x, y, grid)) {
+        std::cout << "\n Enter starting coordinates in the following form: x y ";
+        std::cin >> x >> y; 
+        // Check for valid starting square coordinates, then place starting square
+        if (valid_square(x, y, grid)) {
+            grid[x].replace(y, 1, STARTING_SQUARE);
+        } else {std::cout << "INVALID STARTING SQUARE\n";}
+    }
+    print_grid(grid);
+    x = 0; y = 0; // Reset x and y to invalid coordinates
 
+    while (!valid_square(x, y, grid)) {
+        std::cout << "\n Enter goal coordinates in the following form: x y ";
+        std::cin >> x >> y; 
+        // Check for valid goal coordinates, then place goal square
+        if (valid_square(x, y, grid)) {
+            grid[x].replace(y, 1, GOAL_SQUARE);
+        } else {std::cout << "INVALID GOAL SQUARE\n";}
+    }
+    print_grid(grid);
+    x = 0; y = 0; // Reset x and y to invalid coordinates
+    
     while(!goal) {
-
+        
     }
 
     return 0;
